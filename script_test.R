6b953dfe77f3ba6a49164b84d4e0f71424e91aeb
dimmin <pub269@hotmail.fr>
2018-02-28 10:10:20 +0100

    Adding comment from local liberty.

--- script_test.R
+++ script_test.R
@@ -1,1 +1,3 @@
 # Hello. This is a piece of code written from GitHub.com. Can you see it locally?
+
+# Yes I was able to clone the repo, and now I am writting an additional comment. Can you see it from GitHub.com?
