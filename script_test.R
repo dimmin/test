38d3f8dae7cc5370b6b62b0f7689cefcfa2be3f7
dimmin <pub269@hotmail.fr>
2018-02-28 10:19:22 +0100

    Try#2

--- script_test.R
+++ script_test.R
@@ -3,3 +3,5 @@
 # Yes I was able to clone the repo, and now I am writting an additional comment. Can you see it from GitHub.com?
 
 # Yes, received from GitHub.com! But you pushed using Git Bash. I added this command. Now can you pull & push from RStudio?
+
+# Trying.
