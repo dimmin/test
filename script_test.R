51b2d8e665d9472265805cfe19a9bf5d4f537177
dimmin <pub269@hotmail.fr>
2018-02-28 10:18:38 +0100

    Update script_test.R

--- script_test.R
+++ script_test.R
@@ -1,3 +1,5 @@
 # Hello. This is a piece of code written from GitHub.com. Can you see it locally?
 
 # Yes I was able to clone the repo, and now I am writting an additional comment. Can you see it from GitHub.com?
+
+# Yes, received from GitHub.com! But you pushed using Git Bash. I added this command. Now can you pull & push from RStudio?
